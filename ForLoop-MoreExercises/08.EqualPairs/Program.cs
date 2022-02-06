cbe968881f51ae038d2c8bf1f213f0ccba52b703
Stefan Rusev <StefanGRusev91@gmail.com>
2022-02-06 15:25:55 +0200

    forLoopMoreExercises

---
+++ ForLoop-MoreExercises/08.EqualPairs/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace _08.EqualPairs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            double difference = 0;
+            double previousSum = 0;
+            double lastSum = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                double firstN = double.Parse(Console.ReadLine());
+                double secondN = double.Parse(Console.ReadLine());
+
+                double currentSum = firstN + secondN;
+
+                if (n == 1)
+                {
+                    lastSum = currentSum;
+                    previousSum = currentSum;
+                    break;
+                }
+
+                if (Math.Abs(previousSum - currentSum) > difference  && i != 1)
+                {
+                    difference = Math.Abs(previousSum - currentSum);
+                }
+                
+                if (i == n)
+                {
+                    lastSum = currentSum;
+                    break;
+                }
+
+                previousSum = currentSum;
+
+            }
+
+            if (lastSum == previousSum)
+            {
+            Console.WriteLine($"Yes, value={previousSum}");
+            }
+            else
+            {
+            Console.WriteLine($"No, maxdiff={difference}");
+            }
+        }
+    }
+}
